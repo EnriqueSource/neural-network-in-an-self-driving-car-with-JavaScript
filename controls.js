b65b688f36dda52f7cf3e7282e78ce2cd775d46f
EnriqueSource <enriqueml@blogdeenrique.com>
2023-03-19 16:01:51 +0100

    Refactoring Car driving mechanics close #19

--- controls.js
+++ controls.js
@@ -24,7 +24,6 @@ class Controls {
           this.left = true;
           break;
       }
-      console.table(this);
     }
     document.onkeyup = (event) => {
       switch(event.key) {
@@ -41,7 +40,6 @@ class Controls {
           this.left = false;
           break;
       }
-      console.table(this);
     }
   }
 }
